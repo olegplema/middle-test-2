14f76191ec2c89cbaf088ea39e22fb5962604c2d
Oleg Plema <olegplema01@gmail.com>
2024-05-31 11:50:52 +0300

    feat: Added views

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Image
+
+
+def gallery_view(request):
+    categories = Category.objects.all()
+    return render(request, "gallery.html", {"categories": categories})
+
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Image, pk=image_id)
+    return render(request, "image_detail.html", {"image": image})
